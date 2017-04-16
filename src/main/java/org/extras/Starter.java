85e8487c928053ce565dd9d83eb27ebcbbba00e1
A <techtrial4u@gmail.com>
2017-04-16 15:52:12 -0500

    enter

--- src/main/java/org/extras/Starter.java
+++ src/main/java/org/extras/Starter.java
@@ -6,6 +6,7 @@ import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.context.annotation.ComponentScan;
 
 
+
 @SpringBootApplication(scanBasePackages = "org.extras")
 @EnableConfigurationProperties
 public class Starter {
